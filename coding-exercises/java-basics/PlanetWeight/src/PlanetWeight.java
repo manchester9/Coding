a0a14db86bdbe372288f4b2a9cf4c5769e21807b
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-08 19:31:32 -0500

    Add java code for planet weight

---
+++ coding-exercises/java-basics/PlanetWeight/src/PlanetWeight.java
@@ -0,0 +1,25 @@
+package com.java24hours;
+
+public class PlanetWeight {
+    public static void main(String[] arguments)
+    {
+        System.out.println("Your weight on Earth is");
+        double weight = 178;
+        System.out.println(weight);
+
+        System.out.println("Your weight on Mercury is");
+        double mercury = weight * .378;
+        System.out.println(mercury);
+
+        System.out.println("Your weight on Moon is");
+        double moon = weight * .166;
+        System.out.println(moon);
+
+        System.out.println("Your weight on Jupiter is");
+        double jupiter = weight * .22;
+        System.out.println(jupiter);
+
+
+    }
+
+}
