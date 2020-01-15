ff5316747283291e40892142f0801fc27967e9f3
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-14 20:14:55 -0500

    Add java code to change ascii to character

---
+++ coding-exercises/java-basics/AsciiToCharacter/src/AsciiToCharacter.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class AsciiToCharacter {
+
+    public static void main(String[] args) {
+
+        char ch = 'P';
+        int asciiCode = ch;
+        // type casting char as int
+        int asciiValue = (int)ch;
+
+        System.out.println("ASCII value of "+ch+" is: " + asciiCode);
+        System.out.println("ASCII value of "+ch+" is: " + asciiValue);
+    }
+}
