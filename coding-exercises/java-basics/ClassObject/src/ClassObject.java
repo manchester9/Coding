aa3d49c243e83f317f8a954458a0efe455b13327
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-26 08:06:59 -0500

    Add java code for class object

---
+++ coding-exercises/java-basics/ClassObject/src/ClassObject.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class ClassObject {
+
+    int x = 5;
+
+    public static void main(String[] args) {
+        ClassObject myObj = new ClassObject();
+        System.out.println(myObj.x);
+    }
+
+}
