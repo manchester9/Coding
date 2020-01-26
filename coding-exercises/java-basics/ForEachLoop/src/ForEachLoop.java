18dee44237438e5001061a861087b1593747add6
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-26 08:09:03 -0500

    Add java code for each loop

---
+++ coding-exercises/java-basics/ForEachLoop/src/ForEachLoop.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+
+}
