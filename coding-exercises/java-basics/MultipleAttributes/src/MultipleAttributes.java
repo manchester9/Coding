6d1cebb9431610498cf6cb240de891c6feea8be4
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-26 08:12:04 -0500

    Add java code for multiple attributes

---
+++ coding-exercises/java-basics/MultipleAttributes/src/MultipleAttributes.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class MultipleAttributes {
+
+    String fname = "John";
+    String lname = "Doe";
+    int age = 24;
+
+    public static void main(String[] args) {
+        MultipleAttributes myObj = new MultipleAttributes();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Age: " + myObj.age);
+    }
+
+}
