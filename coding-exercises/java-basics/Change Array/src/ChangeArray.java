a6bf57cedfccafed51a42930a46a91a3f085fa0c
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-26 08:05:34 -0500

    Add java code for changing an array

---
+++ coding-exercises/java-basics/Change Array/src/ChangeArray.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class ChangeArray {
+
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+    }
+}
