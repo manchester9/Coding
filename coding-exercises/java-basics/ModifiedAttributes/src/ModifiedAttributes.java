acf3c8dbb74f11fdfd1ac5212d8a96c135745195
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-26 08:11:29 -0500

    Add java code for modified attributes

---
+++ coding-exercises/java-basics/ModifiedAttributes/src/ModifiedAttributes.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class ModifiedAttributes {
+
+    int x;
+
+    public static void main(String[] args) {
+        ModifiedAttributes myObj = new ModifiedAttributes();
+        myObj.x = 40;
+        System.out.println(myObj.x);
+    }
+
+}
