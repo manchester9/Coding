fc909314d2fbdfdcc602e3a5ad96d7d27d8497d8
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-25 23:28:36 -0500

    Add java code for boolean type

---
+++ coding-exercises/java-basics/BooleanType/src/BooleanType.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class BooleanType {
+
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+    }
+
+}
