68a65320b95f713e238d580f6a0fc18a4e0f9b0a
Azfer Saeed <azfer.saeed@mrm.com>
2020-04-05 22:56:14 -0400

    Java code for inheritance examples

---
+++ coding-exercises/java-basics/AdultClass/src/AdultClass.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AdultClass {
+            protected int age = 0;
+        protected String firstname = "firstname";
+        protected String lastname = "lastname";
+        protected String gender = "MALE";
+        protected int progress = 0;
+
+        public Adult() { }
+        public void move() {
+            System.out.println("Moved.");
+        }
+        public void talk() {
+            System.out.println("Spoke.");
+        }
+
+
+
+
+}
